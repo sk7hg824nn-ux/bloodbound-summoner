72fc2cc1a7bde135ca7e0df1bea7e02986715d58
sk7hg824nn-ux <riddlebox677@gmail.com>
2026-08-30 15:02:21 -0400

    Add packed Mother NPC plate for childhood town door.

---
+++ scripts/art/art_town_mother.gd
@@ -0,0 +1,9 @@
+extends Object
+## Packed childhood town layer. Runtime load only. No class_name.
+const MOTHER := (
+	"/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAA4KCw0LCQ4NDA0QDw4RFiQXFhQUFiwgIRokNC43NjMuMjI6QVNGOj1OPjIySGJJTlZYXV5dOEVmbWVabFNbXVn/" +
+	"2wBDAQ8QEBYTFioXFypZOzI7WVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVlZWVn/wAARCADwAKADASIAAhEBAxEB/8QA"
+)
+
+static func pack() -> String:
+	return MOTHER
